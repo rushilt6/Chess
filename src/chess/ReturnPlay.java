0b91da7a3086c4966cf3f6cba65e0193d5663bcb
rushilt6 <rushil.thummaluru@gmail.com>
2024-02-23 17:00:24 -0500

    merge conflicts

--- src/chess/ReturnPlay.java
+++ src/chess/ReturnPlay.java
@@ -1,7 +1,7 @@
 package chess;
 import java.util.*;
 class ReturnPlay {
-	enum Message {ILLEGAL_MOVE, DRAW, 
+	enum Message {ILLEGAL_MOVE, DRAW,
 				  RESIGN_BLACK_WINS, RESIGN_WHITE_WINS, 
 				  CHECK, CHECKMATE_BLACK_WINS,	CHECKMATE_WHITE_WINS, 
 				  STALEMATE};
